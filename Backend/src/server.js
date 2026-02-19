3bc2c41bcb4ca4010feeff717a015242d0ed7993
Aleena Jabeen <aleenajabeen19648@gmail.com>
2026-02-19 20:24:46 +0500

    backend setup added

---
+++ Backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import { app } from './app.js';
+
+dotenv.config(
+    {
+        path:'./.env'
+    }
+);
+
+const port=process.env.PORT || "6000"
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
