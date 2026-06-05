7010629d1d33c48ffead736401dbed5d640750dd
Zeeshan Ahmad <zeeshanahmad8086@gmail.com>
2026-06-06 00:07:52 +0500

    backend deployed url added

--- backend/src/lib/oauth/google.js
+++ backend/src/lib/oauth/google.js
@@ -4,9 +4,11 @@ import { Google } from "arctic";
 const google_client_id=process.env.GOOGLE_CLIENT_ID;
 const google_client_secret=process.env.GOOGLE_CLIENT_SECRET;
 
+const BASE_URL = process.env.BACKEND_URL || "http://localhost:3000";
+
 export const google=new Google(
     google_client_id,
     google_client_secret, 
-    `https://rozgarhub-fyp.onrender.com/api/v1/auth/google/callback`
+    `${BASE_URL}/api/v1/auth/google/callback`
 // we wil create this route to verify after login
 )
