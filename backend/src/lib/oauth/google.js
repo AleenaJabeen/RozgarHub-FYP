f473e9b72ba88c3e0ccbedde11eab07d3da862f4
Aleena Jabeen <aleenajabeen19648@gmail.com>
2026-06-05 18:40:14 +0500

    backend url added

--- backend/src/lib/oauth/google.js
+++ backend/src/lib/oauth/google.js
@@ -7,6 +7,6 @@ const google_client_secret=process.env.GOOGLE_CLIENT_SECRET;
 export const google=new Google(
     google_client_id,
     google_client_secret, 
-    "http://localhost:3000/api/v1/auth/google/callback"
+    `https://rozgarhub-fyp.onrender.com/api/v1/auth/google/callback`
 // we wil create this route to verify after login
 )
